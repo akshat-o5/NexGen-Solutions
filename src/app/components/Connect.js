ea8dfb3826a29b1d9ddfa70599f60f1e7280bcd6
Akshat Gupta <a.akshat903@gmail.com>
2024-02-29 23:16:39 +0530

    files added

--- src/app/components/Connect.js
+++ src/app/components/Connect.js
@@ -15,7 +15,7 @@ function Connect() {
                 <p className="text-lg mb-8">Connect with us for all your business needs and consultancy services. Whether you&apos;re looking to develop a stunning website that reflects your brand identity, create engaging user interfaces and experiences, or devise effective marketing strategies to reach your target audience, we&apos;ve got you covered. Our team of experts is dedicated to understanding your unique requirements and providing tailored solutions to help you achieve your business goals. With our extensive experience and innovative approach, we&apos;ll work closely with you to deliver results that exceed your expectations. Let&apos;s collaborate and take your business to new heights together. Reach out to us today to get started!</p>
             </div>
             <div className="md:w-1/3">
-            <Link href='/about'>
+            <Link href='/contact'>
                     <button className="bg-transparent flex justify-center  hover:bg-purple-800 text-purple-800 font-semibold hover:text-white py-2 px-4 border border-purple-800 hover:border-transparent rounded transition ease-in duration-700 my-16">
                         Connect
                     </button>
